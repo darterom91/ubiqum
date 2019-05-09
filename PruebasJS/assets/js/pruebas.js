391d962cd77161c691b56a96a8b11ca63a211d73
David Artero <d.arterom@gmail.com>
2019-05-09 16:15:25 +0200

    Task 4: modify form with flex

---
+++ PruebasJS/assets/js/pruebas.js
@@ -0,0 +1,14 @@
+function myFunction3() {
+  document.getElementById("demo5").innerHTML = "Escribiendo desde un archivo exterior";
+  document.getElementById("demo5").style.fontStyle = "bold";
+  document.getElementById("demo5").style.fontSize = "2em";
+  document.getElementById("demo5").style.backgroundColor = "black";
+  document.getElementById("demo5").style.color = "red";
+  document.getElementById("demo5").style.textAlign = "center";
+  document.getElementById("demo5").style.padding = "5%";
+  document.getElementById("demo5").style.margin = "5%";
+}
+
+function outputHtml() {
+  document.getElementById("writeHtml").innerHTML = 5+5;
+}
