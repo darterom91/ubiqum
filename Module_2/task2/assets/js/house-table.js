9f3d6c9a626a70be0f050b1de472dfa87f9a3269
David Artero <d.arterom@gmail.com>
2019-05-22 17:38:13 +0200

    Module 2: Task 2 Filtrer tables from option of the select

--- Module_2/task2/assets/js/house-table.js
+++ Module_2/task2/assets/js/house-table.js
@@ -1,11 +1,125 @@
 var datas = JSON.stringify(data);
 var dataForm = JSON.parse(datas);
-var tabla = "";
+var tabla = '';
+var sele = '';
 var filterCheck = [];
-var middleNotNull = "";
+var middleNotNull = '';
 var partyArray = [];
+var stateArray = [];
+var stateArrayOption = [];
+var op = 0;
+var aux = [];
 var cont = 0;
 
+//Select state
+function notDuplicate() {
+  cont = 0;
+  //Rellena todas las posiciones array aux
+  for (var i = 0; i < dataForm.results[0].members.length; i++) {
+    aux[i] = dataForm.results[0].members[i].state;
+  }
+  console.log('length aux = ' + aux.length);
+
+  //Busca todos los valores i y comprueba si hay iguales en la j
+  //si los hay borra los valores de la array aux
+  for (var i = 0; i < aux.length; i++) {
+    for (var j = 0; j < aux.length - 1; j++) {
+      if (i < j) {
+        if (aux[i] == aux[j]) {
+          delete aux[j];
+          aux.slice(j, 1);
+        }
+      }
+    }
+    cont++;
+  }
+  //comprueba los valores de la array no son undefined,
+  // para luego meter cada valor en la array stateArray
+  console.log('length aux = ' + aux.length);
+  cont = 0;
+  for (var i = 0; i < aux.length; i++) {
+    if (aux[i] != undefined) {
+      console.log('Aux [' + cont + ']: ' + aux[i]);
+      stateArray[cont] = aux[i];
+      cont++;
+    }
+  }
+  console.log('length stateArray = ' + stateArray.length);
+  //se llama a la funcion
+  selectOptions();
+}
+//rellena los option con los valores de la array stateArray
+function selectOptions() {
+  selectOptions = '';
+  selectOptions += '<option>ALL</option>';
+  for (var i in stateArray) {
+    selectOptions +=
+      "<option class='options' value='" +
+      stateArray[i] +
+      "'>" +
+      stateArray[i] +
+      '</option>';
+  }
+  document.getElementById('selectState').innerHTML = selectOptions;
+}
+
+function selectSelectionOptions() {
+  stateArrayOption = document.querySelectorAll('.options');
+  for (var i = 0; i < stateArrayOption.length; i++) {
+    if (stateArrayOption[i].selected == true) {
+      console.log('i = [' + i + '] value: ' + stateArrayOption[i].value);
+      op = stateArrayOption[i].value;
+    }
+  }
+  console.log(stateArrayOption.length);
+  houseTablesSelectFiltre();
+}
+
+function houseTablesSelectFiltre() {
+  tabla = '';
+  // Cabecera de la tabla th
+
+  tabla +=
+    '<thead><tr><th>All name</th>' +
+    '<th>Party</th>' +
+    '<th>state</th>' +
+    '<th>seniority</th>' +
+    '<th>%</th>' +
+    '</tr></thead>';
+
+  //cuerpo de  la tabla td
+  for (var i in dataForm.results[0].members) {
+    // console.log("i = ["+i+"]");
+    if (dataForm.results[0].members[i].state == op) {
+      if (dataForm.results[0].members[i].middle_name == null) {
+        middleNotNull = ' ';
+      } else {
+        middleNotNull = dataForm.results[0].members[i].middle_name;
+      }
+
+      tabla +=
+        "<tbody><tr><td><a href='" +
+        dataForm.results[0].members[i].url +
+        "'>" +
+        dataForm.results[0].members[i].first_name +
+        ' ' +
+        middleNotNull +
+        ' ' +
+        dataForm.results[0].members[i].last_name +
+        '</a></td>';
+      tabla += '<td>' + dataForm.results[0].members[i].party + '</td>';
+      tabla += '<td>' + dataForm.results[0].members[i].state + '</td>';
+      tabla += '<td>' + dataForm.results[0].members[i].seniority + '</td>';
+      tabla +=
+        '<td>' +
+        dataForm.results[0].members[i].votes_with_party_pct +
+        '</td></tr></tbody>';
+    }
+  }
+  document.getElementById('house-data').innerHTML = tabla;
+}
+
+//tablas
 function houseCheck() {
   filterCheck = document.querySelectorAll(".checkParty");
   partyArray = [];
@@ -26,7 +140,6 @@ function houseCheck() {
 
   if (partyArray.length==0){
     console.log("entra");
-    
     houseTables();
   }
 }
--- Module_2/task2/assets/js/senate-table.js
+++ Module_2/task2/assets/js/senate-table.js
@@ -1,16 +1,128 @@
 var datas = JSON.stringify(data);
 var dataForm = JSON.parse(datas);
 var tabla = "";
+var sele = "";
 var filterCheck = [];
 var middleNotNull = "";
 var partyArray = [];
+var stateArray = [];
+var stateArrayOption = [];
+var op = 0;
+var aux = [];
 var cont = 0;
 
-function houseCheck() {
+//Select state
+function notDuplicate() {
+  cont = 0;
+  //Rellena todas las posiciones array aux
+  for(var i = 0; i<dataForm.results[0].members.length; i++){
+    aux[i] = dataForm.results[0].members[i].state;
+  }
+  console.log("length aux = "+aux.length);
+
+  //Busca todos los valores i y comprueba si hay iguales en la j
+  //si los hay borra los valores de la array aux
+  for (var i = 0; i < aux.length; i++) {
+    for (var j = 0; j < aux.length-1; j++) {
+      if (i < j) {
+          if (aux[i] == aux[j]) {
+            delete aux[j];
+            aux.slice(j, 1);
+            console.log("var Contador = " + cont);
+        }
+      }
+    }
+    cont++;
+  }
+  //comprueba los valores de la array no son undefined,
+  // para luego meter cada valor en la array stateArray
+  console.log('length aux = ' + aux.length);
+  cont = 0;
+  for (var i = 0; i < aux.length; i++) {
+    if(aux[i]!=undefined){
+    console.log("Aux ["+cont+"]: "+aux[i]);
+    stateArray[cont]=aux[i];
+    cont++;
+    }
+  }
+  console.log("length stateArray = " + stateArray.length);
+  //se llama a la funcion
+  selectOptions();
+}
+//rellena los option con los valores de la array stateArray
+function selectOptions() {
+  selectOptions = "";
+  selectOptions += "<option>ALL</option>";
+  for (var i in stateArray) {
+    selectOptions +=
+      "<option class='options' value='"+stateArray[i]+"'>"+stateArray[i]+"</option>";
+  }
+  document.getElementById("selectState").innerHTML = selectOptions;
+}
+
+function selectSelectionOptions() {
+  stateArrayOption = document.querySelectorAll('.options');
+  for (var i = 0; i < stateArrayOption.length; i++) {
+    if (stateArrayOption[i].selected == true) {
+      console.log('i = [' + i + '] value: ' + stateArrayOption[i].value);
+      op = stateArrayOption[i].value;
+    }
+  }
+  console.log(stateArrayOption.length);
+  senateTablesSelectFiltre();
+}
+
+function senateTablesSelectFiltre() {
+  tabla = '';
+  // Cabecera de la tabla th
+
+  tabla +=
+    '<thead><tr><th>All name</th>' +
+    '<th>Party</th>' +
+    '<th>state</th>' +
+    '<th>seniority</th>' +
+    '<th>%</th>' +
+    '</tr></thead>';
+
+  //cuerpo de  la tabla td
+  for (var i in dataForm.results[0].members) {
+    // console.log("i = ["+i+"]");
+    if (dataForm.results[0].members[i].state == op) {
+      if (dataForm.results[0].members[i].middle_name == null) {
+        middleNotNull = ' ';
+      } else {
+        middleNotNull = dataForm.results[0].members[i].middle_name;
+      }
+
+      tabla +=
+        "<tbody><tr><td><a href='" +
+        dataForm.results[0].members[i].url +
+        "'>" +
+        dataForm.results[0].members[i].first_name +
+        ' ' +
+        middleNotNull +
+        ' ' +
+        dataForm.results[0].members[i].last_name +
+        '</a></td>';
+      tabla += '<td>' + dataForm.results[0].members[i].party + '</td>';
+      tabla += '<td>' + dataForm.results[0].members[i].state + '</td>';
+      tabla += '<td>' + dataForm.results[0].members[i].seniority + '</td>';
+      tabla +=
+        '<td>' +
+        dataForm.results[0].members[i].votes_with_party_pct +
+        '</td></tr></tbody>';
+    }
+  }
+  document.getElementById('senate-data').innerHTML = tabla;
+}
+
+//tablas
+function senateCheck() {
   filterCheck = document.querySelectorAll(".checkParty");
   partyArray = [];
   console.log(typeof (filterCheck));
   console.log(filterCheck.length);
+  console.log(dataForm.results[0].members[0].party.length);
   for (var i = 0; i < filterCheck.length; i++) {
     if (filterCheck[i].checked == true) {
       console.log("El value de i = " + i + ": " + filterCheck[i].value);
@@ -21,7 +133,6 @@ function houseCheck() {
       partyArray.splice(i, 1);//bugs al quitar los elementos de la array
       senateTablesFiltre(partyArray);
     }
-    mostrar();
   }
 
   if (partyArray.length == 0) {
@@ -81,8 +192,10 @@ function senateTablesFiltre(partyArray) {
 
 function senateTables() {
   console.log("HouseTable Funtion: " + partyArray[2]);
+
   tabla = "";
   // Cabecera de la tabla th
+
   tabla += "<thead><tr><th>All name</th>" +
     "<th>Party</th>" +
     "<th>state</th>" +
