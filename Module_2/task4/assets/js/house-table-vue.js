75b91f73a5f9b1730467c44a975cf65b4f032670
David Artero <d.arterom@gmail.com>
2019-06-05 16:47:15 +0200

    house-data in vue nd fetch()

--- Module_2/task4/assets/js/house-table-vue.js
+++ Module_2/task4/assets/js/house-table-vue.js
@@ -3,12 +3,16 @@ var myHeaders = {
   method: 'GET',
   headers: { 'X-API-Key': 'GsSYZ2inVKeuH50adSeijtpqabjqjgHOUD2nKFXA'}
 };
-var datas;
-var dataForm;
-var vm;
+var objGlobal = [];
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    members: []
+  }
+});
 
 console.log("LoadData");
-
 let datos = loadData(url, myHeaders);
 datos.then(result => {
   return result;
@@ -16,9 +20,8 @@ datos.then(result => {
 
 console.log("cargarDatos en la let datos");
 datos.then(result => cargarDatos(result));
-datos.then(result => parses());
-datos.then(result => mostrar());
-var objGlobal = [];
+
+// datos.then(result => mostrar());
 
 function loadData(url, myHeaders) {
   let datos = fetch(url, myHeaders)
@@ -29,21 +32,25 @@ function loadData(url, myHeaders) {
 
 function cargarDatos(array) {
   objGlobal = array;
+
+  app.members = array;
   return array;
 }
 
-function parses() {
-  datas = JSON.stringify(objGlobal);
-  dataForm = JSON.parse(datas);
-  vm = new Vue({
-    el: '#app',
-    data : datos
-  })
+function checKboxes() {
+  console.log("checKboxes");
+  var checkboxes = new Vue({
+    el: 'checkboxes',
+    data: {
+      checkedNames: []
+    },
+  });
+  console.log("VALUE: " + checkboxes.checkedNames);
 }
 
 function mostrar(){
-  for (var i = 0; i < dataForm.results[0].members.length; i++) {
-    console.log(dataForm.results[0].members[i].total_votes);
+  for (var i = 0; i < objGlobal.results[0].members.length; i++) {
+    console.log(objGlobal.results[0].members[i].first_name);
   }
 }
 // prueba esto
