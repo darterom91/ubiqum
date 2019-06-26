82425c0b5a594b33801b28e24ace114632b97aa3
David Artero <d.arterom@gmail.com>
2019-06-26 13:45:44 +0200

    install bootstrap and axios

--- Module_3/nysl-mobile/src/main.js
+++ Module_3/nysl-mobile/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+
+
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
