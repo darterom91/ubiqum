a1e649532f536ab9d881091004e7624aaf79c560
David Artero <d.arterom@gmail.com>
2019-06-28 15:13:12 +0200

    modificando cabecera

--- Module_3/nysl-mobile/src/components/Cabecera.vue
+++ Module_3/nysl-mobile/src/components/Cabecera.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="cabecera mb-4 bg-light">
     <b-container>
-      <b-row align-v="center" align-h="start" >
-        <b-col cols="8" sm="8" md=10><h1 class="text-left">NYSL</h1></b-col>
-        <b-col cols="4" sm="4" md=2>
+      <b-row align-v="center" align-h="start" class="pt-2 pb-3" >
+        <b-col cols="8" md="9"><h1 class="text-left">NYSL</h1></b-col>
+        <b-col cols="4" md="2">
           <b-link to="/"><b-img :src="require('../assets/nysl_logo.png')" fluid alt="Responsive image"></b-img></b-link>
         </b-col>
       </b-row>
     </b-container>
+    <NavBar/>
   </div>
 </template>
 
