46ee31e4acfa80ea61be32712be33443acadd808
David Artero <d.arterom@gmail.com>
2019-07-05 11:45:41 +0200

    Game Information: GI-navTeam modify

--- Module_3/nysl-mobile/src/components/GI-NavTeam.vue
+++ Module_3/nysl-mobile/src/components/GI-NavTeam.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="GI-NavTeam">
     <div>
-      <b-nav class="bg-dark border-top p-2 justify-content-center">
-        <b-nav-item class="bg-white" href="#">
+      <b-nav class="bg-white border-top p-2 justify-content-center">
+        <b-nav-item class="bg-dark border-bottom border-dark" href="#">
           <router-link
           :to="{name: 'teams', params: {id: team.id}}"
-          v-for="(team ,index) in posts.teams" :key="index" class="m-3">
-            <b-img v-bind:src="team.shield" style="max-width: 80px; max-height: 40px;" alt="" class="p-1 border border-dark"></b-img>
+          v-for="(team ,index) in posts.teams" :key="index" class="m-1">
+            <b-img v-bind:src="team.shield" style="max-width: 80px; max-height: 45px;" alt="" class="p-1 mt-1 border border-dark bg-white"></b-img>
           </router-link>
 
         </b-nav-item>
