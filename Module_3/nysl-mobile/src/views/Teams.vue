80205e32a2f9fd146fbb264e7a4ccb98fd8ddd37
David Artero <d.arterom@gmail.com>
2019-07-05 13:28:15 +0200

    Game Information: Teams  modify

--- Module_3/nysl-mobile/src/views/Teams.vue
+++ Module_3/nysl-mobile/src/views/Teams.vue
@@ -5,13 +5,23 @@
     <div v-for="(team, Iindex) in posts.teams" :key="Iindex">
       <div v-if="$route.params.id === team.id" class="border border-dark">
         <div class="border border-dark">
-          <h1>{{team.id}} | {{team.nameTeam}}</h1>
+          <h1>{{team.nameTeam}}</h1>
         </div>
         <div class="border border-dark">
-          <b-img v-bind:src="team.shield" alt="" class="p-1 border border-dark"></b-img>
+          <img v-bind:src="team.shield" alt="" class="border border-dark img-fluid">
+        </div>
+        <div>
+          <img cols v-bind:src="team.stadio" alt="" class="border border-dark img-fluid">
         </div>
         <div v-for="(player, Jindex) in team.players" :key="Jindex" class="border border-dark">
-            <li>{{player.dorsal}} || {{player.firstName}}</li>
+          <div v-if="player.dorsal % 2 !=0 " class="text-left d-flex bg-light p-2">
+            <div>{{player.dorsal}}</div>
+            <div class="ml-1">{{player.firstName}}</div>
+          </div>
+          <div v-else class="text-left d-flex bg-secondary text-light p-2">
+            <div>{{player.dorsal}}</div>
+            <div class="ml-1">{{player.firstName}}</div>
+          </div>
         </div>
       </div>
     </div>
